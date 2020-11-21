79db522ad6f0a31c66d0698214669aa1abc603fa
mosed5bfh <73242572+mosed5bfh@users.noreply.github.com>
2020-11-21 19:58:13 +0100

    CalculatorView Externalisiert und MainView neugestaltet.

---
+++ btx8081_BFH_TEAM_RED/src/main/java/view/CalculatorView.java
@@ -0,0 +1,23 @@
+package view;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+import model.CalculatorModel;
+import presenter.CalculatorPresenter;
+
+@Route("calculator")
+public class CalculatorView extends VerticalLayout {
+
+    public CalculatorView() {
+
+
+	CalculatorModel model = new CalculatorModel();
+	CalculatorViewImpl view = new CalculatorViewImpl();
+
+	new CalculatorPresenter(model, view);
+
+	add(view); 
+    }
+
+}
--- btx8081_BFH_TEAM_RED/src/main/java/view/MainView.java
+++ btx8081_BFH_TEAM_RED/src/main/java/view/MainView.java
@@ -1,38 +1,61 @@
 package view;
 
+import com.vaadin.flow.component.HasComponents;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.router.RouterLayout;
+import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.PWA;
 
-import model.CalculatorModel;
-import presenter.CalculatorPresenter;
 
 /**
  * The main view contains a button and a click listener.
  */
-@Route("")
+@Route("main")
+@RouteAlias("")
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
 
-public class MainView extends VerticalLayout {
+public class MainView extends VerticalLayout implements HasComponents, RouterLayout{
 
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    public MainView() {
+	
+	VerticalLayout menu = new VerticalLayout();
+	menu.add(new RouterLink("calulator", CalculatorView.class));
+	Anchor anchorWiki = new Anchor("https://wikipedia.com", "Wikipedia");
+	menu.add(anchorWiki);
+	
+	add(menu);
+	
+	Button buttonOpenCalculator = new Button("Open Calculator");
+	buttonOpenCalculator.addClickListener(e-> {
+	    buttonOpenCalculator.getUI().ifPresent(ui -> ui.navigate("calculator"));
+	});
+	add(buttonOpenCalculator);
 
-	public MainView() {
-        
-    	// Create the model and the Vaadin view implementation
-    	CalculatorModel model = new CalculatorModel();
-    	CalculatorViewImpl view = new CalculatorViewImpl();
-    	// The presenter connects the model and view
-    	new CalculatorPresenter(model, view);
-    	// The view implementation is a Vaadin component
-    	add(view); 
-    	
-    	
- 
-    	
-    	
+	Button buttonOpenDiary = new Button("Open Diary");
+	buttonOpenCalculator.addClickListener(e-> {
+	    buttonOpenCalculator.getUI().ifPresent(ui -> ui.navigate("diary"));
+	});
+	add(buttonOpenDiary);
+
+	
+	Button buttonOpenInstructions = new Button("Open Instruction");
+	buttonOpenCalculator.addClickListener(e-> {
+	    buttonOpenCalculator.getUI().ifPresent(ui -> ui.navigate("instruction"));
+	});
+	add(buttonOpenInstructions);
+	
+	Button buttonOpenEmergency = new Button("Open Emergency");
+	buttonOpenCalculator.addClickListener(e-> {
+	    buttonOpenCalculator.getUI().ifPresent(ui -> ui.navigate("emergency"));
+	});
+	add(buttonOpenEmergency);
+
+
+
+
     }
 }
