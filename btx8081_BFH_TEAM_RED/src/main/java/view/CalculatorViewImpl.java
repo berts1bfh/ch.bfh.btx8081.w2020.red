09c963d56b822bc259963a85ee5d20065b49d91f
mosed5bfh <73242572+mosed5bfh@users.noreply.github.com>
2020-11-22 15:43:44 +0100

    realign lines

--- btx8081_BFH_TEAM_RED/src/main/java/view/CalculatorViewImpl.java
+++ btx8081_BFH_TEAM_RED/src/main/java/view/CalculatorViewImpl.java
@@ -9,16 +9,16 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 
 public class CalculatorViewImpl extends VerticalLayout  implements CalculatorViewInterface {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	
-	private TextField display = new TextField(); // shows current result
-	private List<CalculatorViewListener> listeners = new ArrayList<CalculatorViewListener>();
-	public CalculatorViewImpl() {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+
+    private TextField display = new TextField(); // shows current result
+    private List<CalculatorViewListener> listeners = new ArrayList<CalculatorViewListener>();
+    public CalculatorViewImpl() {
 	display.setReadOnly(true);
 	add(display);
 
@@ -27,47 +27,47 @@ public class CalculatorViewImpl extends VerticalLayout  implements CalculatorVie
 	layout_1.add(createButton("2"));
 	layout_1.add(createButton("3"));
 	add(layout_1);
-	
+
 	HorizontalLayout layout_2 = new HorizontalLayout();
 	layout_2.add(createButton("4"));
 	layout_2.add(createButton("5"));
 	layout_2.add(createButton("6"));
 	add(layout_2);
-	
+
 	HorizontalLayout layout_3 = new HorizontalLayout();
 	layout_3.add(createButton("7"));
 	layout_3.add(createButton("8"));
 	layout_3.add(createButton("9"));
 	add(layout_3);
-	
+
 	HorizontalLayout layout_4 = new HorizontalLayout();
 	layout_4.add(createButton("/"));
 	layout_4.add(createButton("-"));
 	layout_4.add(createButton("+"));
 	add(layout_4);
-	
+
 	HorizontalLayout layout_5 = new HorizontalLayout();
 	layout_5.add(createButton("*"));
 	layout_5.add(createButton("C"));
 	layout_5.add(createButton("="));
 	add(layout_5);
-	
+
 	HorizontalLayout layout_6 = new HorizontalLayout();
-    layout_6.add(createButton("0"));
-    add(layout_6);
-	
-	}
-	private Button createButton(String operation) {
+	layout_6.add(createButton("0"));
+	add(layout_6);
+
+    }
+    private Button createButton(String operation) {
 	return new Button(operation, event -> {
-	for (CalculatorViewListener listener : listeners)
-	listener.buttonClick(event.getSource().getText().charAt(0));
+	    for (CalculatorViewListener listener : listeners)
+		listener.buttonClick(event.getSource().getText().charAt(0));
 	});
-	}
-	public void setDisplay(double value) {
+    }
+    public void setDisplay(double value) {
 	display.setValue(Double.toString(value));
-	}
-	public void addListener(CalculatorViewListener listener) {
+    }
+    public void addListener(CalculatorViewListener listener) {
 	listeners.add(listener);
-	}
+    }
 
 }
