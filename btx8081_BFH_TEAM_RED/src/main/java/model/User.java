82f527a4a7656c74e07ae0e6267671501b499851
kilcs3 <73250898+kilcs3@users.noreply.github.com>
2020-11-23 19:31:12 +0100

    test comit1
    test push

--- btx8081_BFH_TEAM_RED/src/main/java/model/User.java
+++ btx8081_BFH_TEAM_RED/src/main/java/model/User.java
@@ -1,5 +1,24 @@
 package model;
 
 public class User {
+	private String lastName;
+	private String firstName;
+	private int patientNr=0;
+	
+	
+	public User (String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		addPatientNr();
+	}
+	
+	public String toString () {
+		return this.firstName+","+this.lastName;
+	}
+	
+	public int addPatientNr() {
+		patientNr += patientNr;
+		return patientNr;
+	}
 
 }
