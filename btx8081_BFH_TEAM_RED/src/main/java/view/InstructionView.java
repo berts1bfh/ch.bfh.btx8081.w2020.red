64a4ca19dadb122528db4b96aa9ea45ef0f15618
mosed5bfh <73242572+mosed5bfh@users.noreply.github.com>
2020-12-04 23:47:15 +0100

    dummy View für Verwaltung von Instruction

--- btx8081_BFH_TEAM_RED/src/main/java/view/InstructionView.java
+++ btx8081_BFH_TEAM_RED/src/main/java/view/InstructionView.java
@@ -1,5 +1,9 @@
 package view;
 
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -8,16 +12,32 @@ import com.vaadin.flow.router.Route;
 @Route("instruction")
 public class InstructionView  extends VerticalLayout{
     
-    
+    Span viewTitle = new Span("Instruktionen");
+
     public InstructionView() {
-
-
+	
+	
 	//TODO: InstructionModel model = new InstructionModel();
 	InstructionViewImpl view = new InstructionViewImpl();
-
+	
 	//TODO: new InstructionPresenter(model, view);
 
-	add(view); 
+	VerticalLayout v1 = new VerticalLayout();
+	VerticalLayout v2 = new VerticalLayout();
+	
+	v2.getStyle().set("border", "1px solid #94949E");
+	v2.add(view); 
+	v1.add(viewTitle);
+	v1.add(v2); 
+	add(v1);
+	
+	Button buttonOpenInstrictionAddView = new Button("bearbeiten", VaadinIcon.EDIT.create());
+	buttonOpenInstrictionAddView.addClickListener(e-> {
+	    buttonOpenInstrictionAddView.getUI().ifPresent(ui -> ui.navigate("InstructionMgmtView"));
+	    buttonOpenInstrictionAddView.setIconAfterText(false);
+	});
+	add(buttonOpenInstrictionAddView);
+	
     }
 
 }
