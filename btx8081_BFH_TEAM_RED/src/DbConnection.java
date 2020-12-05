48ccfbc9d9aff588bf3a3dfad21bec8eacd43343
kilcs3 <73250898+kilcs3@users.noreply.github.com>
2020-12-05 18:26:24 +0100

    the instructionmodel, noch nicht fertig
    InstructionModel.java

---
+++ btx8081_BFH_TEAM_RED/src/DbConnection.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+	
+	
+	public static Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:src/acadiaDB.db"); // connecting to our
+			con.setAutoCommit(false);																				// database
+			System.out.println("Connected!");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e + "");
+		}
+
+		return con;
+	}
+}
