d33840cbacd91b80557a2c1796da003baf1cb005
berts1bfh <simonmichael.bertschinger@students.bfh.ch>
2020-12-12 09:41:29 +0100

    Changed InstructionModel

--- btx8081_BFH_TEAM_RED/src/main/java/model/InstructionModel.java
+++ btx8081_BFH_TEAM_RED/src/main/java/model/InstructionModel.java
@@ -1,22 +1,15 @@
-package main.java.model;
+package model;
 
+public class InstructionModel {
 
-
-
-public class instructionModel<InstruktionSql> {
-	
-	
-	
 	private String instructionTitle; 
 	private String instructionText;
 	private int instructionID;
 	
-	
-	public instructionModel(int instructionID, String instructionTitle, String instructionText) {
+	public InstructionModel(int instructionID, String instructionTitle, String instructionText) {
 		this.instructionTitle = instructionTitle;
 		this.instructionText = instructionText;
 		this.instructionID = instructionID;
-		
 	}
 	
 	public int creatInstruction( String title, String txt) {
@@ -24,27 +17,25 @@ public class instructionModel<InstruktionSql> {
 		instructionText = txt; 
 		instructionTitle = title; 
 		return instructionID;
-		
-			}
+	}
 	
 	public String getInstructionFromDB (int ID, String title) {
-		if (instructionID == ID && title.equals(instructionTitle)){
+		if (instructionID == ID && title.equals(instructionTitle)) {
 			
-		}return getInstructionText();
+		}
+		return getInstructionText();
 	}
 	
 	public String changeInstruction(int ID, String title, String txt) {
-		getInstructio(ID, title); 
+		getInstructionFromDB(ID, title);
 		creatInstruction(txt, title);
 		return instructionText; 
 	}
 	
-	public String deleteInstruction (int ID, String title) {
-		openInstruction (ID, title); 
+	public void deleteInstruction (int ID, String title) {
 		instructionID = 0; 
 		instructionText = null; 
-		instructionTitle = null; 
-		return "instruction deleted"; 
+		instructionTitle = null;
 	}
 
 	public void saveToDB() {
@@ -55,28 +46,20 @@ public class instructionModel<InstruktionSql> {
 		return instructionTitle;
 	}
 
-
 	public void setInstructionTitle(String instructionTitle) {
 		this.instructionTitle = instructionTitle;
 	}
-
 
 	public String getInstructionText() {
 		return instructionText;
 	}
 
-
 	public void setInstructionText(String instructionText) {
 		this.instructionText = instructionText;
 	}
 
-
 	@Override
 	public String toString() {
 		return "instructionModel [instructionTitle=" + instructionTitle + ", instructionText=" + instructionText + "]";
-	} 
-	
-	
-	
-	
+	}
 }
