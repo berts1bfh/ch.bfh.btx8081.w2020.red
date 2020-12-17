28815375d7ea65d961f60250f116ae9b90d77ec2
kilcs3 <73250898+kilcs3@users.noreply.github.com>
2020-12-17 09:11:32 +0100

    Create instructionModel1.java

---
+++ btx8081_BFH_TEAM_RED/src/main/java/model/instructionModel1.java
@@ -0,0 +1,71 @@
+package main.java.model;
+
+
+
+
+
+public class instructionModel1 implements DB {
+	
+	private String instructionTitle; 
+	private String instructionText;
+	private int instructionID;
+	
+	public String getInstructionTitle() {
+		return instructionTitle;
+	}
+
+	public void setInstructionTitle(String instructionTitle) {
+		this.instructionTitle = instructionTitle;
+	}
+
+	public String getInstructionText() {
+		return instructionText;
+	}
+
+	public void setInstructionText(String instructionText) {
+		this.instructionText = instructionText;
+	}
+	
+	public void setInstructionID(int instructionID) {
+		this.instructionID = instructionID;
+	}
+	
+	public int getInstructionID() {
+		return instructionID;
+	}
+	@Override
+	public String toString() {
+		return "instructionModel [instructionTitle=" + instructionTitle + ", instructionText=" + instructionText + "]";
+	}
+
+	@Override
+	public void insertInstruktion() {
+		getInstructionText(); 
+		getInstructionTitle(); 
+		getInstructionID();
+		
+	}
+	
+
+	@Override
+	public void showInstruktion() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void editInstruktion() {
+		
+		
+	}
+
+	@Override
+	public void deleteInstruktion() {
+		// TODO Auto-generated method stub
+		
+	} 
+	
+	
+	
+	
+}
